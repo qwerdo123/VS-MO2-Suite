8b2a65c68611a2df3cff8d9a5bbcea0d02c49dbf
qwerdo123 <qwerdo123@users.noreply.github.com>
2025-07-11 20:49:51 +0200

    Added pathing to the VintagestoryData folder

--- plugins/basic_games/games/game_vintagestory.py
+++ plugins/basic_games/games/game_vintagestory.py
@@ -1,13 +1,14 @@
 from PyQt6.QtCore import QDir
 from ..basic_game import BasicGame
+import os
 
 class VintageStoryGame(BasicGame):
     Name = "Vintage Story Support Plugin"
     Author = "Qwerdo"
-    Version = "1.0"
+    Version = "1.1"
     
     GameName = "Vintage Story"
     GameShortName = "VintageStory"
     GameBinary = "Vintagestory.exe"
     GameDataPath = "Mods"
-    
+    GameDocumentsDirectory = os.getenv("APPDATA") + "/VintagestoryData"
